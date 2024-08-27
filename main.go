022ea4e6cb17e74f1b42ca83f7edee865153af69
MwebeCoder27 <mwebraymond13@gmail.com>
2024-08-27 10:56:42 +0300

    adds scraping functionality

--- main.go
+++ main.go
@@ -1,5 +1,45 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/gocolly/colly"
+)
+
+type Stock struct {
+	company, price, change string
+}
+
 func main() {
+	ticker := []string{
+		"MSFT",
+		"IBM",
+		"GE",
+		"UNP",
+		"COST",
+		"MCD",
+		"V",
+		"WMT",
+		"MMM",
+		"AXP",
+		"AAPL",
+		"BA",
+		"CSCO",
+		"GS",
+		"JPM",
+		"CRM",
+		"VZ",
+	}
+
+	stocks := []Stock{}
+
+	c := colly.NewCollector()
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Print("Visiting", r.URL)
+	})
+	c.OnError(func(_ *colly.Response, err error) {
+		log.Println("Something went wrong", err)
+	})
 
 }
