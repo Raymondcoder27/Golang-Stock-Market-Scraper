48582ed44ae479d146d36e8fa28edfc74a0db906
MwebeCoder27 <mwebraymond13@gmail.com>
2024-08-27 11:04:27 +0300

    adds scraping functionality

--- main.go
+++ main.go
@@ -42,4 +42,17 @@ func main() {
 		log.Println("Something went wrong", err)
 	})
 
+	c.OnHTML("div#quote-header-info", func(e *colly.HTMLElement) {
+		stock := Stock{}
+		stock.company = e.ChildText("h1")
+		fmt.Println("Company", stock.company)
+		stock.price = e.ChildText("fin-streamer[data-field='regularMarketPrice']")
+		fmt.Println("Pice", stock.price)
+		stock.change = e.ChildText("fin-streamer[data-field='regularMarketChangePercent']")
+		fmt.Println("Change", stock.change)
+
+		stocks = append(stocks, stock)
+	})
+
+	c.Visit("https://finance.yahoo.com/qutote/" + t + "/")
 }
